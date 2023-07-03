d82b0912bf394ca40f52192ef9dcec1ba9fa7716
jsericksk <jsericksk@gmail.com>
2023-07-03 16:06:00 -0300

    refactor: Pequena mudança no retorno de signUp e login use case

--- app/src/main/java/com/kproject/quotes/domain/usecase/auth/LoginUseCase.kt
+++ app/src/main/java/com/kproject/quotes/domain/usecase/auth/LoginUseCase.kt
@@ -2,7 +2,8 @@ package com.kproject.quotes.domain.usecase.auth
 
 import com.kproject.quotes.commom.ResultState
 import com.kproject.quotes.domain.model.auth.Login
+import kotlinx.coroutines.flow.Flow
 
 fun interface LoginUseCase {
-    suspend operator fun invoke(login: Login): ResultState<Unit>
+    suspend operator fun invoke(login: Login): Flow<ResultState<Unit>>
 }
--- app/src/main/java/com/kproject/quotes/domain/usecase/auth/SignUpUseCase.kt
+++ app/src/main/java/com/kproject/quotes/domain/usecase/auth/SignUpUseCase.kt
@@ -2,7 +2,8 @@ package com.kproject.quotes.domain.usecase.auth
 
 import com.kproject.quotes.commom.ResultState
 import com.kproject.quotes.domain.model.auth.SignUp
+import kotlinx.coroutines.flow.Flow
 
 fun interface SignUpUseCase {
-    suspend operator fun invoke(signUp: SignUp): ResultState<Unit>
+    suspend operator fun invoke(signUp: SignUp): Flow<ResultState<Unit>>
 }
