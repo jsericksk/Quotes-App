a97054f8fc2a0bc4c977ec6efef73e35002cbdb9
jsericksk <jsericksk@gmail.com>
2023-07-03 16:35:16 -0300

    feat: Criada a MyApplication

--- app/src/main/java/com/kproject/quotes/presentation/MainActivity.kt
+++ app/src/main/java/com/kproject/quotes/presentation/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.kproject.quotes.presentation.navigation.NavigationGraph
 import com.kproject.quotes.presentation.theme.QuotesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
