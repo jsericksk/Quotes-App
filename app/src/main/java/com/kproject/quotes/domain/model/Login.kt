95386a096c7cc5c74c8e2f1f4aadd9684e7b44f8
jsericksk <jsericksk@gmail.com>
2023-07-02 21:06:08 -0300

    feat: Criadas classes de model de domain de Login e SignUp

---
+++ app/src/main/java/com/kproject/quotes/domain/model/Login.kt
@@ -0,0 +1,6 @@
+package com.kproject.quotes.domain.model
+
+data class Login(
+    val email: String = "",
+    val password: String = ""
+)
---
+++ app/src/main/java/com/kproject/quotes/domain/model/SignUp.kt
@@ -0,0 +1,7 @@
+package com.kproject.quotes.domain.model
+
+data class SignUp(
+    val email: String = "",
+    val username: String = "",
+    val password: String = ""
+)
