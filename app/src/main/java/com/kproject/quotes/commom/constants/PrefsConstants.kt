b5cb5a8680d20bcb8b102f902ef7342f21b80800
jsericksk <jsericksk@gmail.com>
2023-07-03 21:03:44 -0300

    feat: Criado MainViewModel

--- app/src/main/java/com/kproject/quotes/commom/constants/PrefsConstants.kt
+++ app/src/main/java/com/kproject/quotes/commom/constants/PrefsConstants.kt
@@ -3,4 +3,5 @@ package com.kproject.quotes.commom.constants
 object PrefsConstants {
     const val AccessToken = "access_token"
     const val RefreshToken = "refresh_token"
+    const val IsUserLoggedIn = "is_user_logged_in"
 }
