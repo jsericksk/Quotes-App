5091b05a19d0d613d802728c0f97806fab0326c4
jsericksk <jsericksk@gmail.com>
2023-07-03 23:10:41 -0300

    feat: Criado SignUpViewModel

---
+++ app/src/main/java/com/kproject/quotes/presentation/screens/auth/signup/SignUpViewModel.kt
@@ -0,0 +1,121 @@
+package com.kproject.quotes.presentation.screens.auth.signup
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kproject.quotes.commom.ResultState
+import com.kproject.quotes.commom.exception.ValidationState
+import com.kproject.quotes.domain.usecase.auth.SignUpUseCase
+import com.kproject.quotes.domain.usecase.auth.validation.ValidateEmailUseCase
+import com.kproject.quotes.domain.usecase.auth.validation.ValidatePasswordUseCase
+import com.kproject.quotes.domain.usecase.auth.validation.ValidateRepeatedPasswordUseCase
+import com.kproject.quotes.domain.usecase.auth.validation.ValidateUsernameUseCase
+import com.kproject.quotes.presentation.screens.auth.utils.toAuthErrorMessage
+import com.kproject.quotes.presentation.screens.auth.utils.toErrorMessage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val signUpUseCase: SignUpUseCase,
+    private val validateUsernameUseCase: ValidateUsernameUseCase,
+    private val validateEmailUseCase: ValidateEmailUseCase,
+    private val validatePasswordUseCase: ValidatePasswordUseCase,
+    private val validateRepeatedPasswordUseCase: ValidateRepeatedPasswordUseCase
+) : ViewModel() {
+    var uiState by mutableStateOf(SignUpUiState())
+        private set
+
+    var signUpState: ResultState<Unit>? by mutableStateOf(null)
+        private set
+
+    fun onUiEvent(event: SignUpUiEvent) {
+        when (event) {
+            is SignUpUiEvent.EmailChanged -> {
+                uiState = uiState.copy(email = event.email)
+                validateFieldsWhenTyping()
+            }
+            is SignUpUiEvent.UsernameChanged -> {
+                uiState = uiState.copy(username = event.username)
+                validateFieldsWhenTyping()
+            }
+            is SignUpUiEvent.PasswordChanged -> {
+                uiState = uiState.copy(password = event.password)
+                validateFieldsWhenTyping()
+            }
+            is SignUpUiEvent.RepeatedPasswordChanged -> {
+                uiState = uiState.copy(repeatedPassword = event.repeatedPassword)
+                validateFieldsWhenTyping()
+            }
+            is SignUpUiEvent.OnDismissErrorDialog -> {
+                uiState = uiState.copy(signUpError = false)
+            }
+        }
+    }
+
+    fun signUp() {
+        if (hasValidationError()) {
+            uiState = uiState.copy(validateFieldsWhenTyping = true)
+        } else {
+            uiState = uiState.copy(isLoading = true)
+            viewModelScope.launch {
+                val signUpResult = signUpUseCase(uiState.toSignUpModel())
+                signUpResult.collect { result ->
+                    when(result) {
+                        is ResultState.Loading -> {
+                            uiState = uiState.copy(isLoading = true)
+                        }
+                        is ResultState.Success -> {
+                            signUpState = ResultState.Success()
+                            uiState = uiState.copy(isLoading = false)
+                        }
+                        is ResultState.Error -> {
+                            result.exception?.let { exception ->
+                                uiState = uiState.copy(
+                                    isLoading = false,
+                                    signUpError = true,
+                                    signUpErrorMessage = exception.toAuthErrorMessage()
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hasValidationError(): Boolean {
+        val usernameValidationState = validateUsernameUseCase(uiState.username)
+        uiState = uiState.copy(usernameError = usernameValidationState.toErrorMessage())
+        val emailValidationState = validateEmailUseCase(uiState.email)
+        uiState = uiState.copy(emailError = emailValidationState.toErrorMessage())
+        val passwordValidationState = validatePasswordUseCase(uiState.password)
+        uiState = uiState.copy(passwordError = passwordValidationState.toErrorMessage())
+        val repeatedPasswordValidationState = validateRepeatedPasswordUseCase(
+            uiState.password, uiState.repeatedPassword
+        )
+        uiState = uiState.copy(
+            repeatedPasswordError = repeatedPasswordValidationState.toErrorMessage()
+        )
+
+        val hasError = listOf(
+            usernameValidationState,
+            emailValidationState,
+            passwordValidationState,
+            repeatedPasswordValidationState
+        ).any { validationState ->
+            validationState != ValidationState.Success
+        }
+
+        return hasError
+    }
+
+    private fun validateFieldsWhenTyping() {
+        if (uiState.validateFieldsWhenTyping) {
+            hasValidationError()
+        }
+    }
+}
