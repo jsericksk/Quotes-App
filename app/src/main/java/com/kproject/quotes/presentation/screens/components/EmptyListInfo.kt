05e9243a7010dae95214cde20fa8a3f4f960fac3
jsericksk <jsericksk@gmail.com>
2023-07-05 10:34:21 -0300

    feat: Criado componente EmptyListInfo

---
+++ app/src/main/java/com/kproject/quotes/presentation/screens/components/EmptyListInfo.kt
@@ -0,0 +1,63 @@
+package com.kproject.quotes.presentation.screens.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kproject.quotes.R
+import com.kproject.quotes.presentation.theme.PreviewTheme
+
+@Composable
+fun EmptyListInfo(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconResId: Int,
+    title: String
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = iconResId),
+            contentDescription = null,
+            modifier = Modifier.size(120.dp),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontSize = 18.sp,
+            fontWeight = Bold,
+            modifier = Modifier.padding(all = 6.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    PreviewTheme {
+        EmptyListInfo(
+            iconResId = R.drawable.outline_error_outline_24,
+            title = stringResource(id = R.string.error_loading_quotes)
+        )
+    }
+}
